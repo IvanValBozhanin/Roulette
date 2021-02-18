e10e697bf7f8c1a7a8e012386358971634a91311
Ivan Valentinov <ivan156drs@gmail.com>
2021-02-18 17:55:49 +0200

    Updated functions.

--- src/com/company/RouletteGame.java
+++ src/com/company/RouletteGame.java
@@ -1,33 +1,107 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RouletteGame extends JFrame{
-    private JTextField jTextField;
-    private JLabel icon;
-    public RouletteGame(){
+    private final JLabel icon, picked, result;
+    private final JSlider slider;
+    private int percent;
+
+    public RouletteGame() {
+        super( "Main" );
         JFrame f = new JFrame("Roulette");
+        percent = 35;
 
-        setLayout( new FlowLayout() );
+        SliderHandler sliderHandler = new SliderHandler();
+        ButtonHandler buttonHandler = new ButtonHandler();
 
-        jTextField = new JTextField();
-        jTextField.setBounds(10, 10, 50, 20);
+        result = new JLabel();
+        result.setBounds(350, 500, 200, 30);
+        result.setFont(new Font("Serif", Font.ITALIC, 20));
+
+        slider = new JSlider(0, 36, 0);
+        slider.setPaintTicks(true);
+        slider.setPaintTrack(true);
+        slider.setPaintLabels(true);
+        slider.setMajorTickSpacing(3);
+        slider.setMinorTickSpacing(1);
+        slider.addChangeListener(sliderHandler);
+        slider.setOrientation(SwingConstants.VERTICAL);
+        slider.setFont(new Font("Serif", Font.ITALIC, 15));
+        slider.setBounds(10, 10, 200, 400);
+
+        picked = new JLabel();
+        picked.setText("Chosen number: " + 0);
+        picked.setBounds(10, 500, 300, 20);
+        picked.setFont(new Font("Serif", Font.ITALIC, 20));
+
+        setLayout(new FlowLayout());
         icon = new JLabel(new ImageIcon("static.png"));
-        icon.setBounds(150, 100, 780 ,360);
-        add(jTextField);
-        add(icon);
+        icon.setBounds(150, 100, 780, 360);
+
+        JButton button = new JButton("Play");
+        button.setFont(new Font("Serif", Font.ITALIC, 20));
+        button.setBounds(650, 500, 100, 30);
+        button.addActionListener(buttonHandler);
+
         {
-            f.add(jTextField);
+            f.add(result);
+            f.add(button);
+            f.add(slider);
+            f.add(picked);
             f.add(icon);
             f.setSize(800, 600);
             f.setLocationRelativeTo(null);
             f.setLayout(null);
             f.setVisible(true);
         }
-        for(long i=0l;i<5000000000l; ++i){
+    }
 
+    private class SliderHandler implements ChangeListener{
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            picked.setText("Chosen number: " + slider.getValue());
         }
-        icon.setIcon(new ImageIcon("rotating.gif"));
+    }
+
+    private class ButtonHandler implements ActionListener{
+        private final Timer timer;
+
+        public ButtonHandler(){
+            timer = new Timer(2000 + (int)(Math.random()*2000), e -> {
+                icon.setIcon(new ImageIcon("static.png"));
+
+                int n = (int)(Math.random()*36);
+                int prob = (int)(Math.random()*100);
+                //picked.setText(" " + percent + " " + prob);
+                if(prob <= percent){
+                    n = slider.getValue();
+                    if(percent > 40) percent -= 20;
+                    else if(percent > 20) percent -= 10;
+                    else if(percent > 10) percent -= 2;
+                }
+                if(slider.getValue() == n){
+                    result.setText("You guessed right!");
+                } else {
+                    result.setText("Roulette number: " + n);
+                    if(percent < 10) percent += 10;
+                    else if(percent < 30) percent += 7;
+                    else if(percent < 50) percent += 3;
+                }
+                slider.setValue(0);
+            });
+            timer.setRepeats(false);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            icon.setIcon(new ImageIcon("rotating.gif"));
+            timer.start();
+        }
     }
 }
