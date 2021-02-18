944ca2a96b7023cdb483e0a7e84339ed76c5f92f
Ivan Valentinov <ivan156drs@gmail.com>
2021-02-18 17:55:53 +0200

    Updated functions.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,8 @@
 package com.company;
 
-import javax.swing.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        RouletteGame rouletteGame = new RouletteGame();
-
+        new RouletteGame();
     }
 }
